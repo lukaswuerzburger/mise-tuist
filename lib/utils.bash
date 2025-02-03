0791f9d848ed8fff307b09ae4223b3abf85f586a
Lukas Würzburger <lukas.w@me.com>
2025-02-03 20:59:23 +0100

    Update utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -35,7 +35,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	url="$GH_REPO/releases/download/${version}/tuist.zip"
+	url="$GH_REPO/archive/refs/tags/${version}.zip"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
